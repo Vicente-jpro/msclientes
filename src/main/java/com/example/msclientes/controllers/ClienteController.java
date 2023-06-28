9bcef49711c9d28fb54342445b7afd5153939c0d
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-06-28 23:53:45 +0100

    Dynamic url added to cliente controller

--- src/main/java/com/example/msclientes/controllers/ClienteController.java
+++ src/main/java/com/example/msclientes/controllers/ClienteController.java
@@ -1,9 +1,11 @@
 package com.example.msclientes.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.msclientes.models.Cliente;
 import com.example.msclientes.services.ClienteService;
@@ -27,18 +30,36 @@ public class ClienteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody Cliente cliente) {
-		return this.clienteService.salvar(cliente);
+	public ResponseEntity salvar(@RequestBody Cliente cliente) {
+		Cliente clt = this.clienteService.salvar(cliente);
+
+		// Usado para apresentar rotas dinamicas.
+		// EX: http://localhost:8080/clientes?bi=12345LA049
+		URI headerLocation = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.query("bi={bi}")
+				.buildAndExpand(clt.getBi())
+				.toUri();
+		return ResponseEntity.created(headerLocation).build();
 	}
 
 	@PatchMapping("/{id_cliente}")
 	// @ApiOperation("Atualizar cliente.")
 	// @ApiResponse(code = 201, message = "Cliente atualizado com sucesso.")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente atualizar(@RequestBody Cliente cliente, @PathVariable("id_cliente") Integer IdCliente) {
+	public ResponseEntity atualizar(@RequestBody Cliente cliente, @PathVariable("id_cliente") Integer IdCliente) {
 		Cliente client = this.clienteService.getCliente(IdCliente);
 		cliente.setIdCliente(client.getIdCliente());
-		return this.clienteService.salvar(cliente);
+		Cliente clt = this.clienteService.salvar(cliente);
+
+		// Usado para apresentar rotas dinamicas.
+		// EX: http://localhost:8080/clientes?bi=12345LA049
+		URI headerLocation = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.query("bi={bi}")
+				.buildAndExpand(clt.getBi())
+				.toUri();
+		return ResponseEntity.created(headerLocation).build();
 	}
 
 	@GetMapping
